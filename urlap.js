36065cf93a8045ff61bd9f19d11e2871d6a689c4
Fricosz <halaszpatrik1234@gmail.com>
2023-05-12 00:24:42 +0200

    80% státusz

--- urlap.js
+++ urlap.js
@@ -3,6 +3,8 @@ let BirthError = document.getElementById('birthError');
 let EmailError = document.getElementById('emailError');
 let PasswordError = document.getElementById('passwordError');
 
+let score = 0;
+
 let ToolTipText = document.querySelector('.tooltip-text');
 let Kerdojel = document.querySelector('.fa-question');
 
@@ -64,7 +66,6 @@ function EmailValidation()
     InputField.style.borderBottom = "1px solid green";
     return true;
 
-
 }
 
 function FormValidation()
@@ -113,8 +114,315 @@ function start()
 
 }
 
+function CheckAnswers()
+{
+    //Gomb
+
+    let gomb = document.getElementById('CheckAnswers');
+
+
+    // Radio button ellenőrzés
+
+    let isRadioValid = false;
+
+    let radio = document.querySelector('input[name="prog"]:checked');
+
+    let RadioValue = radio.value;
+
+    const RadioAnswer = "python"
+
+
+    if(RadioValue === RadioAnswer)
+    {
+        console.log("Python")
+        isRadioValid = true;
+        score++;
+    }
+    else
+    {
+        isRadioValid = false;
+    }
+
+    console.log("RADIO",isRadioValid)
+
+    //Checkbox ellenőrzés
+
+    let isCheckBoxValid = false;
+
+
+    let GoodCheckBoxes = document.querySelectorAll('input[name="check"]');
+    let BadCheckBoxes = document.querySelectorAll('input[name="rossz"]');
+
+    console.log("good",GoodCheckBoxes)
+
+    if (GoodCheckBoxes[0].checked && GoodCheckBoxes[1].checked) {
+        isCheckBoxValid = true;
+        score++;
+    }
+    else
+    {
+        isCheckBoxValid = false;
+    }
+
+    for (let i = 0; i < BadCheckBoxes.length; i++) {
+        if (BadCheckBoxes[i].checked) {
+            isCheckBoxValid = false;
+            break;
+        }
+    }
+
+    console.log("CHECKBOX",isCheckBoxValid)
+
+    //Text ellenőrzés
+
+    let isTextValid = false;
+
+    const TextValue = document.getElementById("TextValasz").value.trim();
+
+    let TextAnswer = "1996";
+
+    //TextAnswer = TextAnswer.replace(/\s+/g, ' ').trim();
+
+    if(TextValue === TextAnswer)
+    {
+        isTextValid = true;
+        score++;
+    }
+    else {
+        isTextValid = false;
+    }
+
+    console.log("TEXT",isTextValid)
+
+
+
+    //Textarea ellenőrzés
+
+    let isTextAreaValid = false;
+
+    let megadottValasz = document.getElementById('textarea').value;
+    let elfogadottValasz = "objektum orientált programozás";
+
+    megadottValasz = megadottValasz.replace(/\s+/g, ' ').trim().toLowerCase();
+
+    if (megadottValasz === elfogadottValasz) {
+        document.getElementById('textarea').style.border = "2px solid green";
+        score++;
+        isTextAreaValid = true;
+    } else {
+        document.getElementById('textarea').style.border = "2px solid red";
+        isTextAreaValid = false;
+    }
+
+    console.log("TEXTAREA", isTextAreaValid)
+
+
+    //GOMB, INPUTOK LETILTÁSA
+    gomb.disabled = true;
+
+    let Radio = document.querySelectorAll('input[name="prog"]');
+
+    for(let i = 0; i < Radio.length; i++)
+    {
+        Radio[i].disabled = true;
+    }
+
+    let GoodBoxes = document.querySelectorAll('input[name="check"]');
+    let BadBoxes = document.querySelectorAll('input[name="rossz"]');
+
+    for(let i = 0; i < GoodBoxes.length; i++)
+    {
+        GoodBoxes[i].disabled = true;
+    }
+
+    for(let i = 0; i < BadBoxes.length; i++)
+    {
+        BadBoxes[i].disabled = true;
+    }
+
+
+    const Text = document.getElementById('TextValasz')
+
+    Text.disabled = true;
+
+    const TextArea = document.getElementById('textarea')
+
+    TextArea.disabled = true;
+
+    const FirstSelect = document.getElementById("kiegeszito1");
+
+    FirstSelect.disabled = true;
+
+    const SecondSelect = document.getElementById("kiegeszito2");
+
+    SecondSelect.disabled = true;
+
+    Select();
+
+    //VÉGEREDMÉNY KIÍRATÁS
+
+    const vegeredmeny = document.getElementById('vegeredmeny');
+    const MaxPontszam = 6;
+
+
+    const SzazelakosArany = Math.round((score / MaxPontszam) * 100)
+
+    vegeredmeny.innerText = "\nEredmény: 6/" + score + " pont\n"+"Százalékos arány: " + SzazelakosArany + "%";
+
+}
+
+function Select()
+{
+    let isFirstSelectValid = false;
+
+
+    const ValasztottElem = document.getElementById("kiegeszito1");
+    let ValasztottErtek = ValasztottElem.value;
+    console.log(ValasztottErtek)
+    let Valasz = "React"
+    const eredmenyElem = document.getElementsByClassName('eredmeny')[0];
+
+    if (ValasztottErtek !== "") {
+        eredmenyElem.textContent = "Válasz: " + ValasztottErtek;
+        isFirstSelectValid = true;
+    }
+    else{
+        eredmenyElem.textContent = "Nincs kiválasztva.";
+        isFirstSelectValid = false;
+    }
+
+    if(ValasztottErtek === Valasz)
+    {
+        score++;
+    }
+
+    console.log("ELSŐ",isFirstSelectValid)
+
+    //Második választás
+
+    let isSecondSelectValid = false;
+
+    const ValasztottElem2 = document.getElementById("kiegeszito2");
+    let ValasztottErtek2 = ValasztottElem2.value;
+    let MasodikValasz = "MySQL"
+    const eredmenyElem2 = document.getElementsByClassName('eredmeny')[1];
+
+    if (ValasztottErtek2 !== "") {
+        eredmenyElem2.textContent = "Válasz: " + ValasztottErtek2;
+        isSecondSelectValid = true;
+
+    } else {
+        eredmenyElem2.textContent = "Nincs kiválasztva.";
+        isSecondSelectValid = false;
+    }
+
+    if(ValasztottErtek2 === MasodikValasz)
+    {
+        score++;
+    }
+
+    console.log("MÁSODIK",isSecondSelectValid)
+
+
+}
+
+function CheckAll() {
+
+    let isRadioChecked = false;
+    let isCheckBoxGoodChecked = false;
+    let isCheckBoxBadChecked = false;
+    let isTextValid = false;
+    let isTextAreaValid = false;
+    let isFirstSelectValid = false;
+    let isSecondSelectValid = false;
+
+    let gomb = document.getElementById('CheckAnswers');
+
+    // Elemek referencia megszerzése
+    const option = document.querySelectorAll('input[name="prog"]')
+    const checkbox = document.querySelectorAll('input[name="check"]')
+    const checkbox2 = document.querySelectorAll('input[name="rossz"]')
+    const text = document.getElementById('TextValasz')
+    const textarea = document.getElementById('textarea')
+
+    const FirstSelect = document.getElementById("kiegeszito1");
+    const SecondSelect = document.getElementById("kiegeszito2");
+
+    for (let i = 0; i < option.length; i++) {
+        if (option[i].checked) {
+            isRadioChecked = true;
+            break;
+        }
+    }
+
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            isCheckBoxGoodChecked = true;
+        }
+        else
+        {
+            isCheckBoxGoodChecked = false;
+        }
+    }
+
+    for (let i = 0; i < checkbox2.length; i++) {
+        if (checkbox2[i].checked) {
+            isCheckBoxBadChecked = true;
+        }
+        else
+        {
+            isCheckBoxBadChecked = false;
+        }
+    }
+
+    if (text.value.trim() === "") {
+        isTextValid = false;
+    }
+    else
+    {
+        isTextValid = true;
+    }
+
+    if(textarea.value.trim() === "")
+    {
+        isTextAreaValid = false;
+    }
+    else
+    {
+        isTextAreaValid = true;
+    }
+
+    if(FirstSelect.value == "")
+    {
+        isFirstSelectValid = false;
+    }
+    else {
+        isFirstSelectValid = true;
+    }
+    if(SecondSelect.value == "")
+    {
+        isSecondSelectValid = false;
+    }
+    else
+    {
+        isSecondSelectValid = true;
+    }
+    if(isRadioChecked && isTextValid && isTextAreaValid && isFirstSelectValid && isSecondSelectValid &&(isCheckBoxBadChecked || isCheckBoxGoodChecked))
+    {
+        gomb.disabled = false;
+    }
+    else
+    {
+        gomb.disabled = true;
+    }
+
+}
+
 window.onload
 {
     console.log("betöltve.");
     Kerdojel.style.visibility = "hidden";
+    let gomb = document.getElementById('CheckAnswers');
+    gomb.disabled = true;
+
 }
